8c789eca018dc54adaf2f71305b75d2226a89897
gkfla16 <gkfla16@gmail.com>
2019-11-29 15:01:38 +0900

    data.frame

--- 20191129.R
+++ 20191129.R
@@ -76,4 +76,58 @@ rownames(score)
 colnames(score)
 colnames(score)[2]
 
+#Data Frame 생성
+city <- c("seoul", "Tokyo", "Washington")
+rank <- c(1,3,2)
+city.info <- data.frame(city, rank)
+city.info
 
+name <- c("Hong", "Kim", "Lee")
+age <- c(22,20,25)
+gender <- factor(c("M","F", "M"))
+blood.type = factor(c("A","O","B"))
+person.info <- data.frame(name, age, gender, blood.type)
+person.info
+
+person2.info <- data.frame(name = c("Hong", "Kim","Lee"),
+                           age = c(22,20,25),
+                           gender = factor(c("M","F","M")),
+                           blood.type = factor(c("A", "O","B")))
+person2.info
+
+city.info[1,1]
+city.info[1,]
+city.info[,1]                    #처음 변수내용
+city.info[ city.info$city, ]     #city전체내용
+city.info[, "rank"]
+
+person.info$name
+person.info[person.info$name == "Hong",]                      #만족하는것에 대한 관측치
+person.info[person.info$name == "Hong", c("name", "age")]     #만족하는 것에 name과 age
+
+data()
+
+iris
+
+iris[, c(1:2)]
+iris[, c(1,3,5)]
+iris[, c("Sepal.Length", "Species")]
+iris[1:5]
+iris[1:5, c(1,3) ]
+
+#Matrix 와 Data Frame에서 사용하는 함수
+dim(person.info)               #**많이 사용하는 함수 - 관측치와 변수의 수 알려줌/// 처음수 = 관측치 수, 나중수 = 변수 수
+dim(iris)
+nrow(person.info)                 
+nrow(m3)
+ncol(person.info)
+ncol(m3)
+head(iris)                     #**많이 사용하는 함수 - 앞에 6개(자동) 
+tail(iris)                     #**많이 사용하는 함수 - 뒤에 6개(자동)
+str(iris)                      #**많이 사용하는 함수 - data가 어떻게 저장되어졌는지 알아보는 것./ data저장형식, 관측치수, 변수의 수 / 
+str(city.info)                                      #- $로 표시된것 - 변수명 : num은 숫자형. (이 타입이 어떤건지 잘 확인해야한다.!!)
+str(person.info)                                    #data.frame으로 만들면 name처럼 factor형으로 자동 바꿔버리는 경우가 있다. 그러니 꼭 str로 확인.!!!
+unique(iris[,5])               #중복된 데이터를 하나씩만 뽑아라
+table(iris[, "Species"])               #**많이 사용하는 함수
+table(person.info[ , "blood.type"])    #종류별로 카운트를 세주는 함수. A형 한개, B형 한개, O형 한개. 
+table(person.info[, "gender"])         #F한명, M두명
