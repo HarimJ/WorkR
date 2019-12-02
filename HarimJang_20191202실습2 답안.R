ca71585d50428e7f3b727282efd8b63f6a5c81eb
Kim-EunMin <58218596+Kim-EunMin@users.noreply.github.com>
2019-12-02 10:21:51 +0900

    실습2 답안

---
+++ HarimJang_20191202실습2 답안.R
@@ -0,0 +1,265 @@
+#
+#장하림
+#20191129 / 20191202
+
+#문1)
+#1
+score <- matrix(c(10,40,60,20,21,60,70,30), nrow = 4, ncol = 2);    score
+colnames(score) <- c("m","f");                                      score
+
+#2
+colnames(score) <- c("male", "female");   score
+
+#3
+score[2,]
+
+#4
+score[,2]
+
+#5
+score[3,2]
+
+#문2)R에서 제공하는 state.x77 데이터셋을 이용하여 작성
+
+#1. state.x77을 변환하여 st에 data frame으로 작성
+st <- as.data.frame(state.x77)
+
+#2. st의 내용을 출력
+st
+
+#3. st의 열 이름 출력
+variable.names(st)
+  #the anser
+names(st)
+colnames(st)
+
+#4. st의 행 이름 출력
+row.names(st)
+
+#5. st의 행의 개수와 열의 개수 출력
+dim(st)
+
+#6. st의 요약 정보 출력
+str(st)
+
+#7. st의 행별 합계와 평균 출력
+apply(st[1:50,],1, sum)
+apply(st[1:50, ],1, mean)
+ rowSums(st)  # way2
+ rowMeans(st) # way2
+ 
+#8. st의 열별 합계와 평균 출력
+apply(st[, 1:8],2, sum)
+apply(st[, 1:8],2, mean)
+ colSums(st)  #way2
+ colMeans(st) #way2
+
+#9. Florida 주의 모든 정보 출력
+st["Florida",]
+
+#10. 50개 주의 수입(Income) 정보만 출력
+st[,"Income"]
+
+#11. Texas 주의 면적(Area)을 출력
+st["Texas", "Area"]
+
+#12. Ohio 주의 인구(Population)와 수입(Income) 출력
+st["Ohio",c("Population","Income")]
+
+#13. 인구가 5,000 이상인 주의 데이터만 출력
+st.1 <- subset(st, Population >= 5000); st.1          #st.1 이라는 변수 굳이 안줘도 된다. 
+  
+#14. 수입이 4,500 이상인 주의 인구, 수입, 면적을 출력
+st.2 <- subset(st,Income >= 4500); st.2
+st.2[,c("Population","Income","Area")]
+
+   #강사님 답안 
+subset( st, Income >= 4500, select = c( 'Income','Area' ) ) # 방법1
+subset( st, Income >= 4500 )[ , c( 'Income', 'Area' ) ] # 방법2
+
+#15. 수입이 4,500 이상인 주는 몇 개인지 출력
+length(rownames(st.2))
+
+   #강사님 답안
+nrow( subset( st, Income >= 4500 ) )    #nrow:수를 세는 함수
+
+
+#16. 전체 면적(Area)이 100,000 이상이고, 결빙일수(Frost)가 120 이상인 주의 정보 출력
+st.3 <- subset(st, Area >= 100000 & Frost >= 120)
+st.3
+
+#17. 인구(Population)가 2,000 미만이고, 범죄율(Murder)이 12미만인 주의 정보 출력
+st.4 <- subset(st, Population < 2000 & Murder < 12)
+st.4
+
+#18. 문맹률(Illiteracy)이 2.0 이상인 주의 평균 수입은 얼마인지 출력
+stt <- subset(st, Illiteracy >= 2.0)
+stt
+mean(stt$Income)
+
+#or
+#stt <- stt$Income            먼저 변수를 벡터로 만들어도 됨
+#mean(stt)
+
+#다른 방법
+mean(subset(st, Illiteracy >= 2.0)$ Income) #한줄로 한눈에 알아보는 방식. 
+
+#19. 문맹률(Illiteracy)이 2.0 미만인 주와 2.0 이상인 주의 평균 수입의 차이 출력
+a <- subset(st, Illiteracy < 2.0); a
+b <- subset(st, Illiteracy >= 2.0); b
+abs(mean(a$Income) - mean(b$Income))
+
+
+  #강사님 답안. 참고
+tmp1 <- subset( st, Illiteracy < 2.0 )
+tmp1 <- tmp1$Income
+tmp2 <- subset( st, Illiteracy >= 2.0 )
+tmp2 <- tmp2$Income
+abs( mean( tmp1 ) - mean( tmp2 ) )
+
+#******20. 기대수명(Life Exp)이 가장 높은 주는 어디인지 출력
+max.life <- max(st$`Life Exp`)
+subset(st, `Life Exp` == max.lif )
+    ## 강사님 답안 
+max.life <- max( st$`Life Exp` )
+rownames( subset( st, `Life Exp` == max.life ) )
+
+
+#21 Pennsylvania 주보다 수입(Income)이 높은 주들 출력
+c <- st["Pennsylvania","Income"]; c
+d <- subset(st,Income > c); d
+row.names(d)
+
+#
+#문3)
+#R에서 제공하는 mtcars 데이터셋은 자동차 모델에 대한 제원 정보를 담고 있다.
+
+#1. 이 데이터셋의 자료구조 출력
+str(mtcars)
+class(mtcars)   #이것도 
+
+#2. 이 데이터셋의 행의 개수와 열의 개수 출력
+dim(mtcars)
+
+#3. 이 데이터셋 열들의 자료형 출력
+str(ncol(mtcars))
+    #강사님 답안 
+str( mtcars )
+
+#*****4. 연비(mpg)가 가장 좋은 자동차 모델 출력
+max.mpg <- max(mtcars$mpg); max.mpg
+subset(mtcars, mpg == max.mpg)
+rownames( subset( mtcars, mpg == max.mpg ) )    #<- 이걸로 사용하기,
+  
+  #강사님 답안
+max.mpg <- max( mtcars$mpg )
+rownames( mtcars[ mtcars$mpg == max.mpg, ] ) # 방법 1
+rownames( subset( mtcars, mpg == max.mpg ) ) # 방법 2
+
+#****5. gear가 4인 자동차 모델 중 연비가 가장 낮은 모델 출력
+u <- subset(mtcars, gear == 4); u
+v <- min(u$mpg); v
+subset(u, mpg == v)
+rownames(subset(u, mpg == v))
+
+  #강사님 답안
+min.mpg <- min( mtcars[ mtcars$gear == 4, 'mpg' ] )
+rownames( mtcars[ mtcars$mpg == min.mpg, ] ) # 방법 1
+rownames( subset( mtcars, mpg == min.mpg ) ) # 방법 2
+
+
+#6. Honda Civic의 연비(mpg)와 gear 수 출력
+mtcars["Honda Civic",c("mpg","gear")]
+
+#*****7. Pontiac Firebird 보다 연비가 좋은 자동차 모델 출력
+s <- mtcars["Pontiac","mpg"]; s
+t <- subset(mtcars, mpg > s); t
+
+   #the answer
+pon.mpg <- mtcars[ 'Pontiac Firebird','mpg' ]
+rownames( mtcars[ mtcars$mpg > pon.mpg, ] ) # 방법 1
+rownames( subset( mtcars, mpg > pon.mpg ) ) # 방법 2
+
+#8. 자동차 모델들의 평균 연비 출력
+mean(mtcars$mpg)
+
+#9. gear의 수 종류 출력
+table(mtcars[,"gear"])
+
+   # the answer
+unique(mtcars$gear)
+
+#
+#문4)
+#R에서 제공하는 airquality 데이터셋은 일별로 대기의 질을 측정한 정보를 담고 있다.
+#
+#1. 이 데이터셋의 자료구조 출력
+str(airquality)
+
+ #the anwser
+class( airquality )
+
+
+#2. 이 데이터셋의 앞쪽 일부분 내용만 출력
+head(airquality)
+
+#3. 기온(Temp)이 가장 높은 날은 언제인지 월(Month)과 일(Day) 출력
+h <- max(airquality$Temp); h
+airquality[h,c("Month","Day")]
+
+  #강사님 답안
+max.temp <- max( airquality$Temp )
+airquality[ airquality$Temp == max.temp, c( 'Month','Day' ) ]       # 방법 1
+subset( airquality, Temp == max.temp, select = c( 'Month','Day' ) ) # 방법 2
+
+
+#4. 6월달에 발생한 가장 강한 바람(Wind)의 세기 출력
+o <- subset(airquality,Month == 6); o
+max(o$Wind)
+
+  #강사님 답안
+max( airquality[ airquality$Month == 6, 'Wind' ] )
+
+#5. 7월 달의 평균 기온(Temp) 출력
+r <- subset(airquality, Month == 7); r
+mean(r$Temp)
+
+  #강사님 답안
+mean( airquality[ airquality$Month == 7, 'Temp' ] )
+
+
+#6. 오존(Ozone) 농도가 100을 넘는 날은 며칠이나 되는지 출력
+q <- subset(airquality, Ozone > 100); q
+length(q)
+ 
+  #강사님 답안
+nrow( subset( airquality, Ozone > 100 ) ) 
+
+#
+#문5)
+#1. R에서 제공하는 state.x77 데이터셋에서 수입(Income)이 5,000 이상인 주의 데이터에서
+#수입(Income), 인구(Population), 면적(Area) 열의 값들만 추출하여 rich_state.csv에 저장
+state.x77
+n <- subset(state.x77, Income >= 5000); n
+n.1 <- n[,c("Income","Population","Area")]
+n.1
+setwd("D:/workR")
+write.csv(n.1,"rich_state.csv", row.names = F)
+
+   #강사님 답안
+setwd( 'd:/WorkR' )
+st <- data.frame( state.x77 ) 
+tmp <- subset( st, Income > 5000, select = c( 'Income', 'Population', 'Area' ) )
+write.csv( tmp, 'rich_state.csv' )
+
+
+
+#2. 1.에서 만든 rich_state.csv파일을 읽어서 ds 변수에 저장한 후 ds 내용 출력
+ds <- read.csv(file = "rich_state.csv")
+ds
+
+   #강사님 답안
+ds <- read.csv( 'rich_state.csv', row.names = T )
+rownames( ds ) <- ds$X
+ds <- ds[ ,-1 ]
+ds
