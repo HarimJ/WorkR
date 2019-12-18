5dc49a0345793238136541e3429a44ae963e9201
gkfla16 <58238503+gkfla16@users.noreply.github.com>
2019-12-19 01:38:53 +0900

    Create 제주청년창업.R

---
+++ 제주청년창업.R
@@ -0,0 +1,58 @@
+#
+# 팀프로젝트 - 제주청년창업
+#
+# 연도별 제주 청년 실업률
+jper <- c(5.1, 5.4, 5.7, 5.1)
+year <- c(2015:2018)
+total <- data.frame(jper, year)
+total$year <- as.factor(total$year)
+head(total)
+library(ggplot2)
+
+# 그래프 그리기
+ggplot(total, aes(x = year, y = jper))+
+  geom_segment(mapping = aes(x = year, xend = year, y = 0, yend = jper), color = "steel blue", size = 3) +
+  geom_point( color = "dark orange", size = 7) +
+  theme_light() +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.x = element_blank()
+  ) +
+  xlab("") +
+  ylab("실업률")+
+  ggtitle(" 제주 청년 실업률")    # 제목으 좀더 두껍게 ????
+
+
+# 종사자, 사업체 수
+setwd("d:/")
+mydata <- read.csv("종사,사업체.csv", as.is = TRUE)
+View(mydata)
+
+mydata <- mydata[,-1]
+colnames(mydata) <- c( "2015","2016","2017","2018")
+rownames(mydata) <- c("a","b","c","d")                      # c("사업체수", "증감률a","종사자수","증감률b")
+head(mydata)
+
+mydata <- t(mydata)
+
+
+al <- factor(c(2015:2018))
+mydata <- data.frame(mydata, al)
+
+install.packages("hrbrthemes")
+library(dplyr)
+library(hrbrthemes)
+
+ggplot(mydata, aes(x = al, y = a))+
+  geom_bar(stat = "identity", fill = ifelse(mydata$al %in% "2018", "dark orange", "grey")) +   # 그래프 사이즈
+  xlab("year") +
+  ylab("사업체수")+
+  ggtitle( " 제주 연도별 사업체 수")    # 이름 굵기
+
+
+ggplot(mydata, aes(x = al, y = c))+
+  geom_bar(stat = "identity", fill = ifelse(mydata$al %in% "2018", "dark orange", "grey"), size = 0.2) +   # 그래프 사이즈
+  xlab("year") +
+  ylab("종사자 수")+
+  ggtitle( " 제주 연도별 종사자 수") 
